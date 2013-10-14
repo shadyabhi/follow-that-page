f97e33da676edd94950cd3972c462da551dfee89
Abhijeet Rastogi <abhijeet.1989@gmail.com>
2013-10-14 16:46:28 +0530

    Initial 30-minute version

---
+++ follow-that-page.py
@@ -0,0 +1,62 @@
+import yaml
+from html2text import html2text
+import urllib2
+import time
+import os
+import hashlib
+import difflib
+
+# Read config file
+config_file = yaml.load(open("conf/websites.yaml"))
+
+cache_dir = config_file['settings']['cache_dir']
+websites_to_monitor = config_file['websites']
+current_time = time.time()
+d = difflib.Differ()
+
+# Create required directories
+if not os.path.exists(cache_dir): os.makedirs(cache_dir)
+
+# Lambda that returns full-path where URL contents are stored.
+# which is: cache_dir + md5sum(url)
+url_path_on_filesystem = lambda url: os.path.join(cache_dir, hashlib.md5(os.path.join(cache_dir + url)).hexdigest())
+
+def read_stored_copy(url):
+    """
+    Read the stored copy of the URL and return it. 
+    If it doesn't exist, return None
+    """
+    path = url_path_on_filesystem(url)
+    if os.path.exists(path):
+        return open(path).read()
+    else:
+        # This should not happen.
+        return None
+
+def write_stored_copy(url, contents):
+    """ Write current page contents to the file """
+    path = url_path_on_filesystem(url)
+
+    with open(path, "w") as f:
+        f.write(contents)
+        
+for w in websites_to_monitor:
+    url = w['url']
+    frequency = w['frequency']
+
+    url_current_copy = html2text(urllib2.urlopen(url).read())
+
+    try:
+        if current_time - os.stat(url_path_on_filesystem(url)).st_mtime < frequency: continue
+    except OSError:
+        # That means that the file doesn't exist. 
+        with open(url_path_on_filesystem(url), 'w') as f:
+            f.write(url_current_copy)
+
+    if url_current_copy == read_stored_copy(url):
+        # No changes
+        os.utime(url_path_on_filesystem(url), (current_time, current_time))
+        pass
+    else:
+        diff = '\n'.join(d.compare(read_stored_copy(url).splitlines(), url_current_copy.splitlines()))
+        write_stored_copy(url, url_current_copy)
